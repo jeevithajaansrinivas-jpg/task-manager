475867f075178a8f9625d1a77295094784d32ab7
jeevithajaansrinivas <jeevithajaansrinivas@gmail.com>
2026-05-17 22:57:14 +0530

    Task Manager Backend

---
+++ routes/taskRoutes.js
@@ -0,0 +1,84 @@
+const express = require("express")
+const Task = require("../models/Task")
+const auth = require("../middleware/authMiddleware")
+
+const router = express.Router()
+
+router.post("/", auth, async (req, res) => {
+
+    try {
+
+        const task = new Task({
+            title: req.body.title,
+            userId: req.user.id
+        })
+
+        await task.save()
+
+        res.status(201).json(task)
+
+    } catch (err) {
+
+        res.status(500).json(err)
+
+    }
+
+})
+
+router.get("/", auth, async (req, res) => {
+
+    try {
+
+        const tasks = await Task.find({
+            userId: req.user.id
+        })
+
+        res.json(tasks)
+
+    } catch (err) {
+
+        res.status(500).json(err)
+
+    }
+
+})
+
+router.put("/:id", auth, async (req, res) => {
+
+    try {
+
+        const updatedTask = await Task.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        )
+
+        res.json(updatedTask)
+
+    } catch (err) {
+
+        res.status(500).json(err)
+
+    }
+
+})
+
+router.delete("/:id", auth, async (req, res) => {
+
+    try {
+
+        await Task.findByIdAndDelete(req.params.id)
+
+        res.json({
+            message: "Task Deleted"
+        })
+
+    } catch (err) {
+
+        res.status(500).json(err)
+
+    }
+
+})
+
+module.exports = router
